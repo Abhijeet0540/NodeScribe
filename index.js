dafcdc0d857f0255856750f1a43b15c9c8f5e3d5
abhi d <abhijeetd439@gmail.com>
2025-03-17 16:48:46 +0530

    upade the code edit and update

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const fs = require('fs');
+const { render } = require('ejs');
 
 
 app.set("view engine", "ejs");
@@ -26,11 +27,34 @@ app.post("/create", (req, res) => {
 
 })
 app.get("/files/:file", (req, res) => {
-    fs.readFile(`./files/${req.params.file}`, 'utf-8',(err, filedata)=>{
-        res.render("show", { filename: req.params.file, data: filedata })
-        
+    fs.readFile(`./files/${req.params.file}`, 'utf-8', (err, filedata) => {
+        if (err) return res.status(500).send("File not found!");
+        else res.render("show", { filename: req.params.file, details: filedata.split('\n')[0], additionaldetails: filedata.split('\n')[1] })
     })
 })
+
+app.get('/edit/:filename', (req, res) => {
+    const filename = req.params.filename;
+    fs.readFile(`./files/${filename}`, 'utf8', (err, data) => {
+        if (err) return res.status(500).send("File not found!");
+        res.render('editForm', { filename:req.params.filename, details: data.split('\n')[0], additionaldetails: data.split('\n')[1] })
+    })
+    
+});
+app.post('/update/:filename', (req, res) => {
+    const { title, details, additional } = req.body;
+    const oldFilename = req.params.filename;
+    const newFilename = title.split(' ').join('') + '.txt';
+    const content = `${details}\n${additional}`;
+
+    fs.rename(`./files/${oldFilename}`, `./files/${newFilename}`, (err) => {
+        if (err) console.log("Rename error:", err);
+        fs.writeFile(`./files/${newFilename}`, content, (err) => {
+            if (err) console.log("Write error:", err);
+            res.redirect('/');
+        });
+    });
+});
 
 
 app.listen(3000)
